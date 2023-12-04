73a0a85301802cb6673f8cec8526e1b0d68ce3b0
KOUY YEM <kouy.yem@camsolution.biz>
2023-12-04 19:05:04 +0700

    Completed App folder and App Bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Footer = () => {
+export function Footer(){
   return (
-    <div>Footer</div>
+    <div className='mt-24'>
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+        © 2023 All rights reserved by Shoppy.com
+      </p>   
+    </div>
   )
 }
 
-export default Footer
+export default Footer;
+
